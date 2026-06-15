2eb303c2ae97ed208d9a2847cb5a93987a3aaee8
Anna Brezdeniuk <abrezden@c2r6p4.42vienna.com>
2026-06-15 18:48:13 +0200

    1

--- push_operations.c
+++ push_operations.c
@@ -6,25 +6,39 @@
 /*   By: abrezden <abrezden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 12:17:50 by abrezden          #+#    #+#             */
-/*   Updated: 2026/06/11 15:01:37 by abrezden         ###   ########.fr       */
+/*   Updated: 2026/06/15 18:47:38 by abrezden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push_top_elements(t_stack **src, t_stack **dest) //тк вершины стека меняются
+/*
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (lst == NULL || new == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}*/
+
+void push_top_elements(t_list **src, t_list **dest)
+{
+    t_stack *head;
+
     if (!src || !dest)
         return ;
-    
+    head = *src;
+    *src = *src->next;
+    head->next = *dest;
+    *dest = head;
 }
 
-void pa(t_stack *a, t_stack *b)
+void pa(t_list *a, t_list *b)
 {
     push_top_elements(a, b);
 }
 
-void pb(t_stack *a, t_stack *b)
+void pb(t_list *a, t_list *b)
 {
     push_top_elements(b, a);
 }
