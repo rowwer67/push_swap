94cfea04a28868bb087b6537a906d4c0ca5c0ec5
Olha Shevchenko <oshevche@c2r2p8.42vienna.com>
2026-06-24 19:31:52 +0200

    main corrected

--- main__.c
+++ main__.c
@@ -5,20 +5,21 @@
 int main(int argc, char **argv)
 {
     int a;
-	t_stack_data basis;
+	t_stack_data *basis;
     t_bench *op_count;
+
 
 
     if (argc <= 1)
         return (0);
-    if (parse_it(argc, argv, &basis))
+    if (parse_it(argc, argv, basis))
         {
             //printing error message 
             // if it returns 2 or 3 - clean the stack
             //abort the programm
         };
-    calculate_disorder(t_stack *a, &basis);
-    if (strat_num == 4)
+    calculate_disorder(basis->stack_a, basis);
+    if (basis->strat_num == 4)
         set_adaptive(&basis);
         
     
