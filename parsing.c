7c2d29e92e7789afb68563d9ed4692440eebc999
Olha Shevchenko <oshevche@c2r1p5.42vienna.com>
2026-06-26 13:01:26 +0200

    parsing.c

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include "push_swap.h"
 
+//returns 1 if strategy was given as parametr
 int	strategy_set(char *s, t_stack_data *basis)
 {
 	if (!s)
@@ -33,7 +34,7 @@ int no_duplicates(int new_el, t_list *lst)
 }
 
 
-//returns 1 if there is --bench setted
+//returns 1 if there is --bench mode setted
 int	mode_set(char *s, t_stack_data *basis)
 {
 	if (!s)
@@ -46,7 +47,6 @@ int	mode_set(char *s, t_stack_data *basis)
 	basis->mode = 0;
 	return (0);
 }
-
 
 // return 0 if everything parsed correctly, otherweise error number
 int    parse_it(size_t argc, char **argv, t_stack_data *basis)
@@ -76,103 +76,3 @@ int    parse_it(size_t argc, char **argv, t_stack_data *basis)
 	}
     return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include "push_swap.h"
-
-static int	is_strategy_flag(char *s)
-{
-	if (ft_strcmp(s, "--simple") == 0)
-		return (1);
-	if (ft_strcmp(s, "--medium") == 0)
-		return (1);
-	if (ft_strcmp(s, "--complex") == 0)
-		return (1);
-	if (ft_strcmp(s, "--adaptive") == 0)
-		return (1);
-	return (0);
-}
-
-static int	set_strategy(char *s, t_stack_data *ps)
-{
-	if (ft_strcmp(s, "--simple") == 0)
-		ps->strat_num = 1;
-	else if (ft_strcmp(s, "--medium") == 0)
-		ps->strat_num = 2;
-	else if (ft_strcmp(s, "--complex") == 0)
-		ps->strat_num = 3;
-	else if (ft_strcmp(s, "--adaptive") == 0)
-		ps->strat_num = 4;
-	else
-		return (0);
-	return (1);
-}
-
-static int	is_mode_flag(char *s)
-{
-	if (ft_strcmp(s, "--bench") == 0)
-		return (1);
-	return (0);
-}
-
-static int	no_duplicates(int new_el, t_list *lst)
-{
-	while (lst)
-	{
-		if (lst->value == new_el)
-			return (0);
-		lst = lst->next;
-	}
-	return (1);
-}
-
-int	parse_it(int argc, char **argv, t_stack_data *ps)
-{
-	int	i;
-	int	new_el;
-	int	strategy_seen;
-
-	i = 1;
-	strategy_seen = 0;
-	while (i < argc)
-	{
-		if (is_mode_flag(argv[i]))
-			ps->mode = 1;
-		else if (is_strategy_flag(argv[i]))
-		{
-			if (strategy_seen)
-				return (1);
-			if (!set_strategy(argv[i], ps))
-				return (1);
-			strategy_seen = 1;
-		}
-		else
-		{
-			if (!ft_atoi_mod(argv[i], &new_el))
-				return (1);
-			if (!no_duplicates(new_el, ps->stack_a))
-				return (1);
-			ft_lstadd_back(&ps->stack_a, ft_lstnew(new_el));
-			ps->st_size++;
-		}
-		i++;
-	}
-	if (ps->st_size == 0)
-		return (1);
-	return (0);
-}
-	*/
